6a0cc9da0c25b8dbe25051f0f3e0865ad7d6806d
Marco La Rosa <m@lr.id.au>
2018-02-08 12:00:57 +1100

    updates to support latest pyramid framework

--- service/README.txt
+++ service/README.txt
@@ -1,14 +1,33 @@
-app README
-==================
+app
+===
 
 Getting Started
 ---------------
 
-- cd <directory containing this file>
+- Change directory into your newly created project.
 
-- $venv/bin/python setup.py develop
+    cd app
 
-- $venv/bin/initialize_app_db development.ini
+- Create a Python virtual environment.
 
-- $venv/bin/pserve development.ini
+    python3 -m venv env
 
+- Upgrade packaging tools.
+
+    env/bin/pip install --upgrade pip setuptools
+
+- Install the project in editable mode with its testing requirements.
+
+    env/bin/pip install -e ".[testing]"
+
+- Configure the database.
+
+    env/bin/initialize_app_db development.ini
+
+- Run your project's tests.
+
+    env/bin/pytest
+
+- Run your project.
+
+    env/bin/pserve development.ini
--- service/setup.py
+++ service/setup.py
@@ -3,42 +3,58 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
 
 requires = [
-    'pyramid',
+    'plaster_pastedeploy',
+    'pyramid >= 1.9a',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
+    'pyramid_retry',
+    'pyramid_tm',
     'SQLAlchemy',
     'transaction',
-    'pyramid_tm',
-    'pyramid_debugtoolbar',
     'zope.sqlalchemy',
     'waitress',
-    ]
+]
 
-setup(name='app',
-      version='0.0',
-      description='app',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='app',
+    version='0.0',
+    description='app',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = app:main',
         ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='app',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = app:main
-      [console_scripts]
-      initialize_app_db = app.scripts.initializedb:main
-      """,
-      )
+        'console_scripts': [
+            'initialize_app_db = app.scripts.initializedb:main',
+        ],
+    },
+)
